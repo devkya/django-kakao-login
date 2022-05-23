bf9a3095205599f79403da64c75fb5c8044b512a
devkya <kihaok@gmail.com>
2022-05-23 14:59:18 +0900

    Success kakao-login

---
+++ backend/accounts/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import kakao_get_login, get_user_info
+urlpatterns = [
+    path('accounts/kakao/login/', kakao_get_login),
+    path('user/kakao/callback/', get_user_info, name="kakao_callback"),
+]
